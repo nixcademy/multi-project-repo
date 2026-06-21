aab1c7c12e891d3b78ec5a150086a96f9a896eb3
Jacek Galowicz <jacek@applicative.systems>
2026-06-21 16:22:50 +0200

    add solution to non-overlay patching

--- release.nix
+++ release.nix
@@ -16,6 +16,28 @@ in
     myapp
     ;
 
+  myapp-patched =
+    let
+      # Helper function to reduce the repetitive patch noise
+      patchLetter =
+        oldLetter: newLetter: drv:
+        drv.overrideAttrs (old: {
+          postPatch = ''
+            substituteInPlace main.cpp \
+              --replace-fail '"${oldLetter}"' '"${newLetter}"'
+          '';
+        });
+    in
+    pkgs.myapp.override {
+      libA = patchLetter "a" "x" (
+        pkgs.libA.override {
+          libC = patchLetter "C" "Z" pkgs.libC;
+          libD = patchLetter "d" "w" pkgs.libD;
+        }
+      );
+      libB = patchLetter "b" "y" pkgs.libB;
+    };
+
   myapp-static = pkgs.pkgsStatic.myapp;
   myapp-win64 = pkgs.pkgsCross.mingwW64.myapp;
   myapp-aarch64 = pkgs.pkgsCross.aarch64-multiplatform.myapp;
